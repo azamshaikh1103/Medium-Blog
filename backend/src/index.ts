de77bf51805d652d627bed7e57effa177afc88fa
azamshaikh1103 <azamshaikh1103@gmail.com>
2024-07-14 03:19:14 +0530

    Connected Prisma with Connection Pool and created Model for user and blog

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,43 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-export default app
+app.post("/api/v1/user/signup", (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  return c.text("Hey there");
+});
+
+app.get("/", (c) => {
+  return c.text("Welcome to homepage");
+});
+
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("Hey there");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("Hey there");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hey there");
+});
+
+app.get("/api/v1/blog", (c) => {
+  return c.text("Hey there");
+});
+
+app.get("/api/v1/blogbulk", (c) => {
+  return c.text("Hey there");
+});
+
+export default app;
